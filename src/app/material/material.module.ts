deb2fc07e07f08f2f7132a7c218fa6cfd90da45e
subrath <subrathkumar.patra@diggibyte.com>
2022-04-13 08:02:54 +0530

    created Header and sidebar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
-
-const materialComponents = [
-  MatButtonModule
-]
+const materialComponents = [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule];
 
 @NgModule({
- 
-  imports: [
-    materialComponents
-  ],
-  exports : [
-    materialComponents
-  ]
+  imports: [materialComponents],
+  exports: [materialComponents],
 })
-export class MaterialModule { }
+export class MaterialModule {}
